51bf65eb4b60a78bab32b68eb7fc6c869e32fda0
sri <maddilakshmisri@gmail.com>
2026-01-25 11:33:25 +0530

    commit

--- app.py
+++ app.py
@@ -811,7 +811,6 @@ def save_quiz():
         
     except Exception as e:
         return {'error': str(e)}, 500
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
